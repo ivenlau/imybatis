bc3b26dbc7db44fd5f343407d3646e257016ffb2
Liu Yin <v-liuyin3@xiaomi.com>
2026-01-11 15:41:25 +0800

    Fix completion issue.

--- src/main/kotlin/com/xiaomi/imybatis/core/completion/SqlCompletionContributor.kt
+++ src/main/kotlin/com/xiaomi/imybatis/core/completion/SqlCompletionContributor.kt
@@ -7,6 +7,7 @@ import com.intellij.patterns.PlatformPatterns
 import com.intellij.psi.*
 import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.psi.xml.XmlFile
+import com.intellij.psi.xml.XmlText
 import com.intellij.util.ProcessingContext
 import com.xiaomi.imybatis.database.MetadataProvider
 
@@ -32,12 +33,19 @@ class SqlCompletionContributor : CompletionContributor() {
             val element = parameters.position
             val xmlFile = element.containingFile as? XmlFile ?: return
 
-            // Check if we're inside a SQL statement
+            // Check if this is a MyBatis mapper file (rootTag must be "mapper")
+            val rootTag = xmlFile.rootTag ?: return
+            if (rootTag.name != "mapper") return
+
+            // Check if we're inside XML text content (not in attributes)
+            val xmlText = PsiTreeUtil.getParentOfType(element, XmlText::class.java)
+            if (xmlText == null) return
+
+            // Check if we're inside a SQL statement tag
             val xmlTag = PsiTreeUtil.getParentOfType(element, com.intellij.psi.xml.XmlTag::class.java) ?: return
             if (xmlTag.name !in listOf("select", "insert", "update", "delete")) return
 
             val project = element.project
-            val text = element.text
 
             // Add SQL keyword completions
             addSqlKeywords(result)
@@ -66,7 +74,7 @@ class SqlCompletionContributor : CompletionContributor() {
         }
 
         private fun addTableCompletions(project: Project, result: CompletionResultSet) {
-            // This will be implemented when database metadata provider is ready
+            // This will be implemented when database support is implemented
             // For now, we can add Entity table names from index
             val indexManager = com.xiaomi.imybatis.core.index.IndexManager.getInstance(project)
             val entities = indexManager.findAllEntities()
